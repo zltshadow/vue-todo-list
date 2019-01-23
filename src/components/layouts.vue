0c566fb728ae1913013c0dc8b3c2663ed195129f
zltshadow <zlt_shadow@163.com>
2019-01-23 17:26:38 +0800

    1.add media source;2.add todo..4 components

---
+++ src/components/layouts.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <!--最外层容器-->
+    <section class="menu">
+      <!--左边的容器 里面加上组件 menus-->
+      <menus></menus> <!-- * -->
+    </section>
+    <section class="content-container">
+      <!--右边的容器 里面加上组件 todo-->
+      <todo></todo>
+    </section>
+  </section>
+</template>
+<script>
+import menus from './menus'
+import todo from './todo'
+export default {
+  components: {
+    menus, // 菜单
+    todo // 待办
+  }
+}
+</script>
+<style lang="less">
+@import "../common/style/layouts.less";
+</style>
